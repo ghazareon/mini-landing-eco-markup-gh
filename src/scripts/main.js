f270bae10dd88b0946b848dc4e63528221d3a949
Hayk Ghazaryan <ghazareon@gmail.com>
2025-01-17 22:11:26 +0400

    feat: add swiper lib & isolation media query viewer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 import $ from "jquery";
+// import Swiper from "swiper";
 
-$("a").on("click", function (e) {
- e.preventDefault();
+// import "swiper/css";
+
+let SwiperTop = new Swiper(".js-running-link", {
+ spaceBetween: 0,
+ centeredSlides: true,
+ speed: 6000,
+ autoplay: {
+  delay: 1
+ },
+ loop: true,
+ slidesPerView: "auto",
+ allowTouchMove: false,
+ disableOnInteraction: true
 });
+
+// $("a").on("click", function (e) {
+//  e.preventDefault();
+// });
+
+// let SwiperTop = new Swiper(".s-3", {
+//  spaceBetween: 0,
+//  centeredSlides: true,
+//  speed: 6000,
+//  autoplay: {
+//   delay: 1
+//  },
+//  loop: true,
+//  slidesPerView: "auto",
+//  allowTouchMove: false,
+//  disableOnInteraction: true
+// });
+
+// console.log("object");
